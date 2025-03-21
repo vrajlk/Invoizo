e73d5ca109ac6a0a6e364f294972846ba44aa8aa
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-03-22 01:14:50 +0530

    check auth for authentication before accessing createshop page

---
+++ server/middleware/Check.auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../utils/generatetoken");
+
+const checkAuth = (req, res) => {
+    try {
+        const token = req.cookies.token; // Get token from cookies
+    
+        if (!token) {
+          return res.json({ authenticated: false, isAdmin: false });
+        }
+    
+        const decoded = jwt.verify(token, JWT_SECRET);
+    
+        res.json({ authenticated: true, isAdmin: decoded.isAdmin });
+      } catch (error) {
+        res.json({ authenticated: false, isAdmin: false });
+      }
+    
+};
+
+module.exports = checkAuth;
+
+
