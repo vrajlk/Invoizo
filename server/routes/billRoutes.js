928b12406a510db2ecae40cf7536c7ab1f2e05e1
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-04-13 02:13:23 +0530

    style: Format routes in billRoutes.js for improved readability and consistency

--- server/routes/billRoutes.js
+++ server/routes/billRoutes.js
@@ -4,9 +4,9 @@ const billController = require('../controller/billController');
 const authMiddleware = require('../middleware/Check.auth');
 
 router.get('/', authMiddleware, billController.getBills);
-router.get('/:id', authMiddleware, billController.getBill);
-router.post('/', authMiddleware, billController.createBill);
-router.put('/:id', authMiddleware, billController.updateBill);
-router.delete('/:id', authMiddleware, billController.deleteBill);
+router.get('/:id',authMiddleware, billController.getBill);
+router.post('/',authMiddleware, billController.createBill);
+router.put('/:id',authMiddleware, billController.updateBill);
+router.delete('/:id',authMiddleware, billController.deleteBill);
 
 module.exports = router;
