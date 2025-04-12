d12240d4eb537e378cae7d3fc5d703508db7ac59
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-04-13 02:13:15 +0530

    feat: Add new route for Dashboard authentication to enhance admin access control

--- server/routes/Authroutes.js
+++ server/routes/Authroutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const registerAdmin = require('../controller/Auth.signup.js');
 const loginAdmin = require('../controller/Auth.login.js');
 const {isAdmin} = require('../middleware/Admin.auth.js');
-
+const Dashauth = require('../middleware/Dashboardauth.js');
 const checkAuth = require('../middleware/Check.auth.js');
 
 router.post('/signup', registerAdmin);
@@ -12,6 +12,8 @@ router.post('/login', loginAdmin);
 
 
 router.get('/check-auth', checkAuth);
+
+router.get('/dashauth', Dashauth);
 
 
 router.get('/dashboard',isAdmin,(req,res) =>{
