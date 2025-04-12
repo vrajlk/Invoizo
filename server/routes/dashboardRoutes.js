a89cd08458ef704610f409d68eee2c876235e715
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-04-13 02:13:29 +0530

    chore: Comment out unused authentication middleware in dashboardRoutes.js for cleaner code

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controller/dashboardController');
 // const authMiddleware = require('../middleware/auth');
+// const authMiddleware = require('../middleware/Admin.auth');
+
 
 router.get('/', dashboardController.getDashboard);
 
