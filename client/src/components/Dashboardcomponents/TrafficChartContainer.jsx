8f409ab6f5d6c0c790cd9d01c285d3e2af7520a0
VRAJ LAKUM <vrajlakum2015@gmail.com>
2026-03-31 22:07:40 +0530

    updated public ip from old ec2 instance to newer ec2 instance

--- client/src/components/Dashboardcomponents/TrafficChartContainer.jsx
+++ client/src/components/Dashboardcomponents/TrafficChartContainer.jsx
@@ -7,7 +7,7 @@ const TrafficChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get("http://65.2.129.154:3000/api/traffic-data"); // your route
+        const res = await axios.get("http://13.204.85.69/:3000/api/traffic-data"); // your route
         const { labels, values } = res.data;
 
         const scaledData = labels.map((label, i) => ({
@@ -46,4 +46,4 @@ const TrafficChart = () => {
 
 export default TrafficChart;
 
-// http://65.2.129.154:3000/api/traffic-data
+// http://13.204.85.69/:3000/api/traffic-data
--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const getTotalRevenue = require('./routes/getTotalRevenueRoute.js');
 app.set("trust proxy", 1);
 
 app.use(cors({
-    origin: 'http://65.2.129.154',
+    origin: 'http://13.204.85.69/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
